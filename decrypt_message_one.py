e3967ce7cb809a404aa821f769e7a44e2c0e684a
RishiVirpura <rishivirpura@gmail.com>
2023-09-10 23:46:04 -0400

    decrypted message one

--- decrypt_message_one.py
+++ decrypt_message_one.py
@@ -41,3 +41,46 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write code below
+
+reverse_cipher = {
+    'v':'a',
+    'h':'b',
+    'r':'c',
+    'j':'d',
+    't':'e',
+    'x':'f',
+    's':'g',
+    'a':'h',
+    'e':'i',
+    'f':'j',
+    'b':'k',
+    'n':'l',
+    'o':'m',
+    'i':'n',
+    'g':'o',
+    'l':'p',
+    'm':'q',
+    'z':'r',
+    'q':'s',
+    'u':'t',
+    'c':'u',
+    'k':'v',
+    'd':'w',
+    'p':'x',
+    'y':'y',
+    'w':'z',
+    '}': ' ',
+    '^': '\n',
+    '5': ',',
+    '[': '!',
+    '-':':',
+    '*':')',
+    '%': '.' 
+}
+
+new_message = ""
+
+for i in encrypted_message:
+    new_message += reverse_cipher[i]
+    
+print(new_message)
