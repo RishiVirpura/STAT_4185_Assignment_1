b542430ad0e3871310007df59cf6dcbbe680064a
RishiVirpura <rishivirpura@gmail.com>
2023-09-11 00:04:34 -0400

    Decrypted message 2 solved

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,22 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+front = 1
+back = -2
+normal = encrypted_message
+backwards = encrypted_message
+decrypted_message = ""
+count = 0
+
+
+while(count < len(encrypted_message)):
+    if count % 2 == 0:
+        decrypted_message += encrypted_message[count]
+    else:
+        decrypted_message += encrypted_message[back]
+        back -= 2
+        
+    count += 1
+
+print(decrypted_message)
